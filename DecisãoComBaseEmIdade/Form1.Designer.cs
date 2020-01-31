0bb4459306959e81c01bddab2bea184ddaf30e92
Carrilhos <gcarrilhos@hotmail.com>
2020-01-31 08:44:57 -0300

    Primeira versão

---
+++ DecisãoComBaseEmIdade/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DecisãoComBaseEmIdade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.categoria = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DataNascimento = new System.Windows.Forms.DateTimePicker();
+            this.Hoje = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(84, 48);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(345, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(55, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Identificar categoria";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // categoria
+            // 
+            this.categoria.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.categoria.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.categoria.Location = new System.Drawing.Point(301, 144);
+            this.categoria.Name = "categoria";
+            this.categoria.Size = new System.Drawing.Size(100, 23);
+            this.categoria.TabIndex = 7;
+            this.categoria.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(57, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Data de nascimento:";
+            // 
+            // DataNascimento
+            // 
+            this.DataNascimento.Location = new System.Drawing.Point(201, 93);
+            this.DataNascimento.Name = "DataNascimento";
+            this.DataNascimento.Size = new System.Drawing.Size(216, 20);
+            this.DataNascimento.TabIndex = 9;
+            this.DataNascimento.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // Hoje
+            // 
+            this.Hoje.AutoSize = true;
+            this.Hoje.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.Hoje.Location = new System.Drawing.Point(184, 19);
+            this.Hoje.Name = "Hoje";
+            this.Hoje.Size = new System.Drawing.Size(61, 13);
+            this.Hoje.TabIndex = 10;
+            this.Hoje.Text = "Dia de hoje";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(455, 187);
+            this.Controls.Add(this.Hoje);
+            this.Controls.Add(this.DataNascimento);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.categoria);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(1920, 1080);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Escolhinha De futsal SHP";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label categoria;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker DataNascimento;
+        private System.Windows.Forms.Label Hoje;
+    }
+}
+
---
+++ DecisãoComBaseEmIdade/Form1.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DecisãoComBaseEmIdade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+   
+
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txtNome.Text == String.Empty)
+               
+            {
+
+                MessageBox.Show("Todos os dados devem ser informados.", "Ateção!", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+
+                TimeSpan tsQuantidadeDias = DateTime.Now.Date - DataNascimento.Value;
+                int idade = (tsQuantidadeDias.Days / 365);
+
+                    if (idade > 17)
+                {
+                    categoria.Text = "Adulto";
+
+                }else if(idade > 13)
+                {
+                    categoria.Text = "Juvenil B";
+
+                }else if(idade > 10)
+                {
+                    categoria.Text = "Juvenil A";
+                
+                }else if(idade > 7)
+                {
+                    categoria.Text = "Infantil B";
+
+                }else if(idade >= 5)
+                {
+                    categoria.Text = "Infantil A";
+
+                }
+                else
+                {
+                    categoria.Text = "Não existe categoria";
+                }
+
+
+
+                
+            }
+        
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            Hoje.Text = "Hoje é " + DateTime.Now.ToShortDateString();
+
+        }
+
+       
+    }
+
+}
